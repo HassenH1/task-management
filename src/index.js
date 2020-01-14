353f62605a9ad6fc7f4709d76f2769a1b0f03655
hassen <hasansaid51@gmail.com>
2020-01-14 09:15:10 -0800

    added store and reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import tasks from './reducers'
+
+const store = createStore(tasks)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
